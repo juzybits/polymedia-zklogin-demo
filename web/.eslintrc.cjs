b6de8ca8ffd75d21d8ea7f8e8c35bf7dda181c3b
juzybits <96890594+juzybits@users.noreply.github.com>
2024-02-07 19:57:16 -0100

    web: move from Parcel to Vite

---
+++ web/.eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2021: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'plugin:react-hooks/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+        '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+        '@typescript-eslint/no-floating-promises': 'off',
+        'react-refresh/only-export-components': [ 'warn', { allowConstantExport: true } ],
+        'react/no-unescaped-entities': 'off',
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+    },
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+}
