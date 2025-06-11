371446436d264fb07d87a9a5f1cfe49d8ab2826b
juzybits <96890594+juzybits@users.noreply.github.com>
2025-06-11 15:40:38 +0200

    moderinze eslint

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,8 +1,10 @@
+import { fixupConfigRules } from "@eslint/compat";
 import eslint from "@eslint/js";
 import stylistic from "@stylistic/eslint-plugin";
-import tsEslint from "typescript-eslint";
+import eslintPluginImport from "eslint-plugin-import";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-import { fixupConfigRules } from "@eslint/compat";
+import unusedImports from "eslint-plugin-unused-imports";
+import tsEslint from "typescript-eslint";
 
 export default [
     eslint.configs.recommended,
@@ -22,27 +24,69 @@ export default [
         },
         plugins: {
             "@stylistic": stylistic,
+            import: eslintPluginImport,
+            "unused-imports": unusedImports,
         },
         rules: {
+            // === stylistic ===
             "@stylistic/jsx-quotes": [ "error", "prefer-double" ],
-            "@stylistic/member-delimiter-style": [ "error", { "multiline": { "delimiter": "semi" }, "singleline": { "delimiter": "semi" } } ],
+            "@stylistic/member-delimiter-style": [ "error", { multiline: { delimiter: "semi" }, singleline: { delimiter: "semi" } } ],
             "@stylistic/no-tabs": "error",
-            "@stylistic/quotes": [ "error", "double", { "avoidEscape": true } ],
+            "@stylistic/quotes": [ "error", "double", { avoidEscape: true } ],
             "@stylistic/semi": [ "error", "always" ],
+
+            // === imports ===
+            // "import/extensions": ["error", "ignorePackages", { ts: "never", tsx: "never" }], // require .js
+            "import/first": "error",
+            "import/newline-after-import": "error",
+            "import/no-duplicates": "error",
+            "import/order": ["error", {
+                "groups": [
+                    "builtin", // Node.js built-in modules (fs, path, etc.)
+                    "external", // npm packages
+                    ["internal", "parent", "sibling", "index"] // project imports (aliased + relative)
+                ],
+                "pathGroups": [
+                    {
+                        "pattern": "nexus-launchpad-sdk",
+                        "group": "external",
+                        "position": "after",
+                    }
+                ],
+                "pathGroupsExcludedImportTypes": ["builtin"],
+                "newlines-between": "always",
+                "alphabetize": {
+                    "order": "asc",
+                    "caseInsensitive": true,
+                }
+            }],
+            "unused-imports/no-unused-imports": "error",
+
+            // === typescript ===
             "@typescript-eslint/consistent-type-definitions": [ "error", "type" ],
             "@typescript-eslint/no-confusing-void-expression": "off",
             "@typescript-eslint/no-floating-promises": "off",
+            "@typescript-eslint/no-inferrable-types": "off",
             "@typescript-eslint/no-misused-promises": "off",
+            "@typescript-eslint/no-namespace": "off",
             "@typescript-eslint/no-non-null-assertion": "off",
             "@typescript-eslint/no-unnecessary-condition": "off",
-            "@typescript-eslint/no-unused-vars": [ "error", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_", "caughtErrorsIgnorePattern": "^_" } ],
+            "@typescript-eslint/no-unnecessary-type-parameters": "off",
+            "@typescript-eslint/no-unused-expressions": [ "error", { allowShortCircuit: true, allowTernary: true } ],
+            "@typescript-eslint/no-unused-vars": [ "error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_", caughtErrorsIgnorePattern: "^_" } ],
             "@typescript-eslint/prefer-nullish-coalescing": "off",
             "@typescript-eslint/restrict-template-expressions": "off",
             "@typescript-eslint/use-unknown-in-catch-callback-variable": "off",
+
+            // === general ===
             "no-constant-condition": "off",
+
+            // === react ===
+            "react/display-name": "off",
             "react/no-unescaped-entities": "off",
             "react/prop-types": "off",
             "react/react-in-jsx-scope": "off",
+
             // Chill with the any checks
             "@typescript-eslint/no-explicit-any": "off",
             "@typescript-eslint/no-unsafe-assignment": "off",
@@ -51,6 +95,12 @@ export default [
             react: {
                 version: "18"
             }
+        },
+    },
+    {
+        files: ["**/__tests__/**/*.ts?(x)", "src/web/src/**/*.ts?(x)", "src/react/src/**/*.ts?(x)"],
+        rules: {
+            "import/extensions": ["off", "ignorePackages"], // don't require .js
         },
     },
 ];
