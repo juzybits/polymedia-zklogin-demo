333e51a759b45723448162e52b2da352d2bd06ab
juzybits <96890594+juzybits@users.noreply.github.com>
2025-06-11 12:31:11 +0200

    replace modal with native dialog

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -11,7 +11,7 @@ import {
     jwtToAddress,
 } from "@mysten/sui/zklogin";
 import { NetworkName, makePolymediaUrl, requestSuiFromFaucet, shortenAddress } from "@polymedia/suitcase-core";
-import { LinkExternal, Modal, isLocalhost } from "@polymedia/suitcase-react";
+import { LinkExternal, isLocalhost } from "@polymedia/suitcase-react";
 import { jwtDecode } from "jwt-decode";
 import { useEffect, useRef, useState } from "react";
 import "./App.less";
@@ -58,7 +58,8 @@ export const App: React.FC = () =>
 {
     const accounts = useRef<AccountData[]>(loadAccounts()); // useRef() instead of useState() because of setInterval()
     const [balances, setBalances] = useState<Map<string, number>>(new Map()); // Map<Sui address, SUI balance>
-    const [modalContent, setModalContent] = useState<string>("");
+    const [modalContent, setModalContent] = useState<string | null>(null);
+    const modalRef = useRef<HTMLDialogElement>(null);
 
     useEffect(() => {
         completeZkLogin();
@@ -66,6 +67,14 @@ export const App: React.FC = () =>
         const interval = setInterval(() => fetchBalances(accounts.current), 5_000);
         return () => {clearInterval(interval);};
     }, []);
+
+    useEffect(() => {
+        if (modalContent && modalRef.current) {
+            modalRef.current.showModal();
+        } else if (!modalContent && modalRef.current) {
+            modalRef.current.close();
+        }
+    }, [modalContent]);
 
     /* zkLogin end-to-end */
 
@@ -244,7 +253,7 @@ export const App: React.FC = () =>
             return null;
         })
         .finally(() => {
-            setModalContent("");
+            setModalContent(null);
         });
 
         if (!zkProofs) {
@@ -317,7 +326,7 @@ export const App: React.FC = () =>
             return null;
         })
         .finally(() => {
-            setModalContent("");
+            setModalContent(null);
         });
     }
 
@@ -400,10 +409,12 @@ export const App: React.FC = () =>
         : ["Google", "Twitch"]; // Facebook requires business verification to publish the app
     return (
     <div id="page">
-
-        <Modal onClose={() => setModalContent("")}>
+        <dialog
+            ref={modalRef}
+            onClose={() => setModalContent(null)}
+        >
             {modalContent}
-        </Modal>
+        </dialog>
 
         <div id="logos">
             <LinkExternal href="https://polymedia.app" follow={true}>
@@ -465,7 +476,7 @@ export const App: React.FC = () =>
                             onClick={() => {
                                 requestSuiFromFaucet(NETWORK, acct.userAddr);
                                 setModalContent("💰 Requesting SUI from faucet. This will take a few seconds...");
-                                setTimeout(() => { setModalContent(""); }, 3000);
+                                setTimeout(() => { setModalContent(null); }, 3000);
                             }}
                         >
                             Use faucet
@@ -491,4 +502,4 @@ export const App: React.FC = () =>
     );
 };
 
-const GitHubLogo: React.FC = () => <svg viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#24292f"/></svg>;
+const GitHubLogo: React.FC = () => <svg viewBox="0 0 98 96" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#24292f"/></svg>;
